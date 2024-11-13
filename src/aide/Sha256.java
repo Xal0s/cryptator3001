6fd283886250fdcaedf2256b1562bdeed015d69a
Xal0s <kevinpereira95@free.fr>
2024-11-13 16:54:06 +0100

    modification texte Sha258

--- src/aide/Sha256.java
+++ src/aide/Sha256.java
@@ -6,7 +6,7 @@ public class Sha256 {
             "SHA-256 prend un message de n'importe quelle longueur.\n" +
             "Il génère une empreinte (ou \"digest\") de 256 bits (32 octets), qui est unique pour chaque message.\n" +
             "L'empreinte est fixe et il est pratiquement impossible de retrouver le message original à partir de l'empreinte. \n" +
-            "De plus, il est très difficile de trouver deux messages différents produisant la même empreinte (ce qu'on appelle une \"collision\")."
+            "De plus, il est impossible de trouver deux messages différents produisant la même empreinte (ce qu'on appelle une \"collision\")."
         );
         AideHachage.afficherAideHachage();
     }
