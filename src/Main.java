9de864018600736594d66565e9edc76c5a79e04a
Benjamin TIsserand <80269986+Turbotard@users.noreply.github.com>
2024-11-13 14:33:02 +0100

    MD5 Added

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
+import hash.MD5;
 import hash.Sha256;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        //System.out.println(MD5.convertirEnMD5("udfydgezyudfgazeyidgzaeitydgfaez55e645464546é4&65é46&5é465&4"));
         //System.out.println(Sha256.ConvertToSha256("izehgfyuozaegfoyuzaegfyuoazegfoyu65145465984&é654&65é4&65é&&&   5448ù$$ù$ù$*ù*ù*ù$*ù^^^^^^^^,;:!"));
     }
 }
