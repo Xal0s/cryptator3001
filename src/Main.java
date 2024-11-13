524c479763277fec3fd063de57640dc4a1c0c05b
Benjamin TIsserand <80269986+Turbotard@users.noreply.github.com>
2024-11-13 14:29:12 +0100

    SHA256 added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+import hash.Sha256;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        //System.out.println(Sha256.ConvertToSha256("izehgfyuozaegfoyuzaegfyuoazegfoyu65145465984&é654&65é4&65é&&&   5448ù$$ù$ù$*ù*ù*ù$*ù^^^^^^^^,;:!"));
     }
 }
