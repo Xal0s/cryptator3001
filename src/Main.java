4cf1fddd24c5774bf6db0805a55df4bf676fb62e
Benjamin TIsserand <80269986+Turbotard@users.noreply.github.com>
2024-11-13 14:38:54 +0100

    uncommented test in Main

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
         // Test de la classe Polybe
-//        String texte = "Hello World";
-//        String code = Polybe.chiffrer(texte);
-//        System.out.println("Code: " + code);
-//        System.out.println("Texte: " + Polybe.dechiffrer(code));
+        String texte = "Hello World";
+        String code = Polybe.chiffrer(texte);
+        System.out.println("Code: " + code);
+        System.out.println("Texte: " + Polybe.dechiffrer(code));
     }
 }
