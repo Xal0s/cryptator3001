55c88cb84271cc0461188d15afe1675defd27d76
doudouux <dounya.berramdane@edu.esiee-it.fr>
2024-11-13 15:37:38 +0100

    Chiffrement et Déchiffrement en Rot(x)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import rotationX.ChiffrementRotation;
+import rotationX.DechiffrementRotation;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        //System.out.println("Hello, World!");
+        //ChiffrementRotation.chiffrementRotation();
+        //DechiffrementRotation.dechiffrementRotation();
+
     }
 }
