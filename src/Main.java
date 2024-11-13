a4d787b006f398f810a8025494611eb18203dc95
Benjamin TIsserand <80269986+Turbotard@users.noreply.github.com>
2024-11-13 15:00:21 +0100

    enlever des choses inutiles

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,5 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         //System.out.println(MD5.convertirEnMD5("udfydgezyudfgazeyidgzaeitydgfaez55e645464546é4&65é46&5é465&4"));
-        //System.out.println(Sha256.ConvertToSha256("izehgfyuozaegfoyuzaegfyuoazegfoyu65145465984&é654&65é4&65é&&&   5448ù$$ù$ù$*ù*ù*ù$*ù^^^^^^^^,;:!"));
     }
 }
