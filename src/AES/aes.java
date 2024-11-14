288711a9f48b0968677af41632d9fd043db17412
Sambachpx <113524274+Sambachpx@users.noreply.github.com>
2024-11-14 11:38:14 +0100

    supp code

--- src/AES/aes.java
+++ src/AES/aes.java
@@ -1,31 +1,10 @@
 package AES;
 
 import java.util.Base64;
-import java.util.Scanner;
 import javax.crypto.Cipher;
-import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 
 public class aes {
-
-    public static void AES(String[] args) throws Exception {
-        // Génère une clé secrète pour l'algorithme AES
-        SecretKey cle = KeyGenerator.getInstance("AES").generateKey();
-        try (Scanner scanner = new Scanner(System.in)) {
-            // Demande à l'utilisateur de saisir le texte à chiffrer
-            System.out.print("Texte à chiffrer : ");
-            String texte = scanner.nextLine();
-
-            // Chiffre le texte saisi par l'utilisateur
-            String chiffre = chiffrer(texte, cle);
-            // Affiche le texte chiffré
-            System.out.println("Chiffré : " + chiffre);
-        } catch (Exception e) {
-            // Affiche un message d'erreur en cas d'exception
-            System.out.println("Erreur : " + e.getMessage());
-        }
-    }
-
     // Méthode pour chiffrer un texte avec une clé secrète
     public static String chiffrer(String texte, SecretKey cle) throws Exception {
         // Initialise le chiffrement AES en mode chiffrement
